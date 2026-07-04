f2c65948bd284ab939dc8fed1c532d38c15e891c
Arpit Ratan <arpit.ratan@outlook.com>
2026-07-04 17:22:36 +0530

    AutoDoc improvements for packages/aip-provider/tests/test_exception.py

--- packages/aip-provider/tests/test_exception.py
+++ packages/aip-provider/tests/test_exception.py
@@ -4,3 +4,4 @@ from aip_provider.exceptions import AIProviderException
 def test_aip_provider_exception_is_subclass_of_exception():
     """Verify that AIProviderException is a subclass of the built-in Exception class."""
     assert issubclass(AIProviderException, Exception)
+
