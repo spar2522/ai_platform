4ca1845d6280ad533f37805cfbda0a7eef4f3529
Arpit Ratan <arpit.ratan@outlook.com>
2026-07-12 21:34:14 +0530

    AutoDoc improvements for packages/aip-canonica/tests/test_public_api.py

--- packages/aip-canonica/tests/test_public_api.py
+++ packages/aip-canonica/tests/test_public_api.py
@@ -4,5 +4,7 @@ from aip_canonica import understand
 
 
 def test_understand_not_implemented():
+    """Verify that the understand function raises a NotImplementedError
+    when called with a dummy file."""
     with pytest.raises(NotImplementedError):
         understand("dummy.xlsx")
