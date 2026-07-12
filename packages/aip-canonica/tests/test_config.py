0a5760668200455c32a9dec289ecf0d9c93277a7
Arpit Ratan <arpit.ratan@outlook.com>
2026-07-12 21:32:44 +0530

    AutoDoc improvements for packages/aip-canonica/tests/test_config.py

--- packages/aip-canonica/tests/test_config.py
+++ packages/aip-canonica/tests/test_config.py
@@ -3,6 +3,12 @@ from aip_canonica.config import get_config
 
 
 def test_configure():
+    """Verify that configure sets the learning_enabled parameter to False."""
     configure(learning_enabled=False)
-
     assert get_config().learning_enabled is False
+
+
+def test_configure_with_true():
+    """Verify that configure sets the learning_enabled parameter to True."""
+    configure(learning_enabled=True)
+    assert get_config().learning_enabled is True
